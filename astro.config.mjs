d696deb41c55e9e3837186daa9ba6304dafb7c81
Benedikt Scheffbuch <benedikt.scheffbuch@inovex.de>
2025-12-20 21:58:51 +0100

    feat: made images responsive

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,11 @@ export default defineConfig({
   build: {
     assets: "assets",
   },
+  image: {
+    layout: "constrained",
+    responsiveStyles: true,
+    breakpoints: [
+      100, 200, 300, 400, 500, 640, 750, 828, 1080, 1280, 1668, 2048, 2560,
+    ],
+  },
 });
